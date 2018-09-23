7a58b7f6eb3020d0bef97dcf869155bf7af1c770
rick <rebelZn*6*>
2018-09-23 15:54:06 -0500

    ADD: test data ADD: icon

--- app/src/main/java/com/school/codes/ric/mobileappsproject/ui/MainActivity.java
+++ app/src/main/java/com/school/codes/ric/mobileappsproject/ui/MainActivity.java
@@ -19,12 +19,23 @@ import android.view.View;
 import com.getbase.floatingactionbutton.FloatingActionButton;
 import com.getbase.floatingactionbutton.FloatingActionsMenu;
 import com.school.codes.ric.mobileappsproject.R;
+import com.school.codes.ric.mobileappsproject.data.AssessmentDAO;
+import com.school.codes.ric.mobileappsproject.data.CourseDAO;
 import com.school.codes.ric.mobileappsproject.data.TermDAO;
+import com.school.codes.ric.mobileappsproject.resource.AssessmentRO;
+import com.school.codes.ric.mobileappsproject.resource.CourseRO;
 import com.school.codes.ric.mobileappsproject.resource.TermRO;
 
+import java.sql.Timestamp;
 import java.text.ParseException;
+import java.util.Calendar;
 import java.util.List;
 import java.util.Objects;
+
+import static com.school.codes.ric.mobileappsproject.util.Constants.ASSESSMENT_TABLE_NAME;
+import static com.school.codes.ric.mobileappsproject.util.Constants.COURSE_TABLE_NAME;
+import static com.school.codes.ric.mobileappsproject.util.Constants.PLAN_STATUS;
+import static com.school.codes.ric.mobileappsproject.util.Constants.TERM_TABLE_NAME;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -42,9 +53,14 @@ public class MainActivity extends AppCompatActivity
     private static final String COURSES_INTENT = "COURSES_INTENT";
     private static final String ASSESSMENTS_INTENT = "ASSESSMENTS_INTENT";
     private static final String TAG = MainActivity.class.getSimpleName();
+
     private FragmentManager manager;
     private NavigationView navigationView;
     private Menu navMenu;
+    private TermDAO termDAO;
+
+    //todo: dynamic button on associate
+    //todo: handle back
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +70,8 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         manager = getSupportFragmentManager();
+
+        loadTestData();
 
         fabItUp();
 
@@ -72,7 +90,6 @@ public class MainActivity extends AppCompatActivity
 
         populateDrawerMenu();
 
-
     }
 
     private void displayHome() {
@@ -88,16 +105,15 @@ public class MainActivity extends AppCompatActivity
 
         List<TermRO> terms = null;
         try {
-            TermDAO termDAO = new TermDAO(getApplicationContext());
             terms = termDAO.getAll();
         } catch (ParseException e) {
             e.printStackTrace();
         }
 
         navMenu.clear();
-        Menu termMenu = navMenu.addSubMenu("Terms");
-        MenuItem courseMenu = navMenu.add("Courses");
-        MenuItem assessmentMenu = navMenu.add("Assessments");
+        Menu termMenu = navMenu.addSubMenu("All Terms");
+        MenuItem courseMenu = navMenu.add("All Courses");
+        MenuItem assessmentMenu = navMenu.add("All Assessments");
 
         assert terms != null;
         for (TermRO t : terms) {
@@ -265,6 +281,172 @@ public class MainActivity extends AppCompatActivity
                 .commit();
     }
 
+    private void loadTestData() {
+
+        //Load test Assessments
+        Timestamp a1GoalDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        a1GoalDate.setDate(30);
+        a1GoalDate.setMonth(0);
+
+        AssessmentRO a1 =
+                new AssessmentRO("Objective 1", AssessmentRO.AssessmentType.OBJECTIVE, a1GoalDate);
+
+        Timestamp a2GoalDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        a2GoalDate.setDate(20);
+        a2GoalDate.setMonth(1);
+
+        AssessmentRO a2 =
+                new AssessmentRO("Performance 1", AssessmentRO.AssessmentType.PERFORMANCE, a2GoalDate);
+
+        Timestamp a3GoalDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        a3GoalDate.setDate(20);
+        a3GoalDate.setMonth(2);
+
+        AssessmentRO a3 =
+                new AssessmentRO("Objective 2", AssessmentRO.AssessmentType.OBJECTIVE, a3GoalDate);
+
+        Timestamp a4GoalDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        a4GoalDate.setDate(22);
+        a4GoalDate.setMonth(3);
+
+        AssessmentRO a4 =
+                new AssessmentRO("Performance 2", AssessmentRO.AssessmentType.PERFORMANCE, a4GoalDate);
+
+        //Load test Courses
+        Timestamp c1StartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c1StartDate.setDate(3);
+        c1StartDate.setMonth(0);
+
+        Timestamp c1EndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c1EndDate.setDate(3);
+        c1EndDate.setMonth(1);
+
+        CourseRO c1 =
+                new CourseRO("Math", c1StartDate, c1EndDate,
+                        PLAN_STATUS, "My mentor", "These are test notes",
+                        c1StartDate, c1EndDate);
+
+        Timestamp c2StartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c2StartDate.setDate(4);
+        c2StartDate.setMonth(1);
+
+        Timestamp c2EndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c2EndDate.setDate(3);
+        c2EndDate.setMonth(6);
+
+        CourseRO c2 =
+                new CourseRO("English", c2StartDate, c2EndDate,
+                        PLAN_STATUS, "My mentor 2", "These are test notes for this course",
+                        c2StartDate, c2EndDate);
+
+        Timestamp c3StartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c3StartDate.setDate(1);
+        c3StartDate.setMonth(7);
+
+        Timestamp c3EndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c3EndDate.setDate(1);
+        c3EndDate.setMonth(9);
+
+        CourseRO c3 =
+                new CourseRO("Programming 101", c3StartDate, c3EndDate,
+                        PLAN_STATUS, "My mentor 3", "These are test notes for this course",
+                        c2StartDate, c3EndDate);
+
+        Timestamp c4StartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c4StartDate.setDate(2);
+        c4StartDate.setMonth(9);
+
+        Timestamp c4EndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        c4EndDate.setDate(13);
+        c4EndDate.setMonth(11);
+
+        CourseRO c4 =
+                new CourseRO("Java Fundamentals", c4StartDate, c4EndDate,
+                        PLAN_STATUS, "My mentor 4", "These are test notes for this course",
+                        c4StartDate, c4EndDate);
+
+        //Load test Terms
+        Timestamp springStartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        springStartDate.setDate(3);
+        springStartDate.setMonth(0);
+
+        Timestamp springEndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        springEndDate.setDate(6);
+        springEndDate.setMonth(4);
+
+        Timestamp fallStartDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        fallStartDate.setDate(1);
+        fallStartDate.setMonth(7);
+
+        Timestamp fallEndDate = new Timestamp(Calendar.getInstance().getTimeInMillis());
+        fallEndDate.setDate(13);
+        fallEndDate.setMonth(11);
+
+        TermRO spring = new TermRO("Spring Term", springStartDate, springEndDate);
+        TermRO fall = new TermRO("Fall Term", fallStartDate, fallEndDate);
+
+        // Add data to DB
+        termDAO = new TermDAO(getApplicationContext());
+        termDAO.clearDB(ASSESSMENT_TABLE_NAME);
+        termDAO.clearDB(COURSE_TABLE_NAME);
+        termDAO.clearDB(TERM_TABLE_NAME);
+
+        CourseDAO courseDAO = new CourseDAO(getApplicationContext());
+        AssessmentDAO assessmentDAO = new AssessmentDAO(getApplicationContext());
+
+        try {
+
+            termDAO.add(spring);
+            spring = termDAO.get(termDAO.getLastId());
+            termDAO.add(fall);
+            fall = termDAO.get(termDAO.getLastId());
+
+            courseDAO.add(c1);
+            c1 = courseDAO.get(courseDAO.getLastId());
+            c1.setTermId(spring.getId());
+            courseDAO.update(c1);
+
+            courseDAO.add(c2);
+            c2 = courseDAO.get(courseDAO.getLastId());
+            c2.setTermId(spring.getId());
+            courseDAO.update(c2);
+
+            courseDAO.add(c3);
+            c3 = courseDAO.get(courseDAO.getLastId());
+            c3.setTermId(fall.getId());
+            courseDAO.update(c3);
+
+            courseDAO.add(c4);
+            c4 = courseDAO.get(courseDAO.getLastId());
+            c4.setTermId(fall.getId());
+            courseDAO.update(c4);
+
+            assessmentDAO.add(a1);
+            a1 = assessmentDAO.get(assessmentDAO.getLastId());
+            a1.setCourseId(c1.getId());
+            assessmentDAO.update(a1);
+
+            assessmentDAO.add(a2);
+            a2 = assessmentDAO.get(assessmentDAO.getLastId());
+            a2.setCourseId(c1.getId());
+            assessmentDAO.update(a2);
+
+            assessmentDAO.add(a3);
+            a3 = assessmentDAO.get(assessmentDAO.getLastId());
+            a3.setCourseId(c2.getId());
+            assessmentDAO.update(a3);
+
+            assessmentDAO.add(a4);
+            a4 = assessmentDAO.get(assessmentDAO.getLastId());
+            a4.setCourseId(c2.getId());
+            assessmentDAO.update(a4);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
@@ -366,4 +548,5 @@ public class MainActivity extends AppCompatActivity
     public void editTerm(int id) {
         addTerm(id);
     }
+
 }
